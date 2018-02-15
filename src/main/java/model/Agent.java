3690a3418f18b7b8b6049a6f4c9c71050660771d
Unknown <pablosky96futbol@hotmail.com>
2018-02-15 18:32:08 +0100

    Modelo implementado
    Cambios en los atributos del modelo y solucionado los fallos del acoplamiento.

---
+++ src/main/java/model/Agent.java
@@ -0,0 +1,115 @@
+package model;
+
+import java.awt.Point;
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Agents")
+public class Agent implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;	
+	private Point localizacion;
+	private String email;
+	private String identificador;
+	private String tipo;
+
+	Agent() {
+	}
+
+	public Agent(String nombre, Point localizacion, String email, String identificador, String tipo) {
+		super();
+		this.nombre = nombre;
+		this.localizacion = localizacion;
+		this.email = email;
+		this.identificador = identificador;
+		this.tipo = tipo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Point getLocalizacion() {
+		return localizacion;
+	}
+
+	public void setLocalizacion(Point localizacion) {
+		this.localizacion = localizacion;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((identificador == null) ? 0 : identificador.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agent other = (Agent) obj;
+		if (identificador == null) {
+			if (other.identificador != null)
+				return false;
+		} else if (!identificador.equals(other.identificador))
+			return false;
+		return true;
+	}	
+	
+	@Override
+	public String toString() {
+		return "Identificador: " + this.identificador;
+	}
+
+	
+
+
+
+}
