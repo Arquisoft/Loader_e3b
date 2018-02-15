531b9255b5c699a3ad7a0ff5f4cf2318c479cb7a
Diego <UO251682@uniovi.es>
2018-02-15 18:14:32 +0100

    Test del Modelo adaptado a Agentes.

--- src/test/java/modeltest/UserTest.java
+++ src/test/java/modeltest/UserTest.java
@@ -2,6 +2,7 @@ package modeltest;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
 import java.util.Date;
 
 import model.User;
@@ -13,55 +14,73 @@ public class UserTest {
 	@Test
 	public void testEquals() {
 		Date date = new Date(System.currentTimeMillis());
-		User user1 = new User("Dani", "Duque", "email@gmail.com", date, "C\\Buenavida", "Español", "7777777R");
-		User user2 = new User("Dani", "Duque", "email@gmail.com", date, "C\\Buenavida", "Español", "7777777R");
-		User user3 = new User("Dani", "Duque", "email@gmail.com", date, "C\\Buenavida", "Español", "7787777R");
-		User user4 = new User("Dani", "Duque", "email@gmail.com", date, "C\\Buenavida", "Español", null);
+		//coordenadas opcionales 
+		Agent agenteCiudadano1 = new Agent("Dani","","dani35@gmail.com","dani123","Ciudadano");
+		Agent agenteCiudadano1_1 = new Agent("Dani","","dani1456@gmail.com","dani123","Ciudadano");
+		Agent agenteCiudadano2 = new Agent("Julio","","julio1235@gmail.com","julio125","Ciudadano");
+		//
+		Agent agenteEntidad1 = new Agent("Entidad 1","entidad123@gmail.com","entidad123","Entidad");
+		Agent agenteEntidad1_1 = new Agent("Entidad 1.1","entidad123567@gmail.com","entidad123","Entidad");
+		Agent agenteEntidad2 = new Agent("Entidad 2","entidad123@gmail.com","entidad567","Entidad");
+		
+		//
+		
+		Agent agentSensor1 = new Agent("Sensor 1",new Point(43,-6),"sensor@gmail.com","sensor123","Sensor");
+		Agent agentSensor1_1= new Agent("Sensor 1.1",new Point(43,-6),"sensor1.1@gmail.com","sensor123","Sensor");
+		Agent agentSensor2= new Agent("Sensor 2",new Point(43,-6),"sensor2@gmail.com","sensor456","Sensor");
+		
+		
+		assertNotNull(agenteCiudadano1);
+		assertNotNull(agenteCiudadano1_1);
+		assertNotNull(agenteCiudadano2);
+		assertNotNull(agenteEntidad1);
+		assertNotNull(agenteEntidad1_1);
+		assertNotNull(agenteEntidad2);
+		assertNotNull(agentSensor1);
+		assertNotNull(agentSensor1_1);
+		assertNotNull(agentSensor2);
 
-		assertEquals(true, user1.equals(user2));
-		assertEquals(true, user3.equals(user3));
-		assertEquals(false, user2.equals(user3));
-		assertNotNull(user1);
-		assertEquals(false, user1.equals(new Integer(8)));
-		assertEquals(false, user4.equals(user3));
+		assertEquals(true, agenteCiudadano1.equals(agenteCiudadano1_1));
+		assertEquals(true, agenteEntidad1.equals(agenteEntidad1_1));
+		assertEquals(true, agentSensor1.equals(agentSensor1_1));
+		
+		
+		assertEquals(false, agenteCiudadano1.equals(agenteCiudadano2));
+		assertEquals(false, agenteEntidad1.equals(agenteEntidad2));
+		assertEquals(false, agentSensor1.equals(agentSensor2));
 	}
 
 	@Test
 	public void testHashCode() {
-		Date date = new Date(System.currentTimeMillis());
-		User user1 = new User("Dani", "Duque", "email@gmail.com", date, "C\\Buenavida", "Español", "7777777R");
-		User user2 = new User("Dani", "Duque", "email@gmail.com", date, "C\\Buenavida", "Español", "7777777R");
-		User user3 = new User("Dani", "Duque", "email@gmail.com", date, "C\\Buenavida", "Español", "7787777R");
-		User user4 = new User("Dani", "Duque", "email@gmail.com", date, "C\\Buenavida", "Español", null);
-		User user5 = new User("Dani", "Duque", "email@gmail.com", date, "C\\Buenavida", "Español", null);
+	
+		Agent agenteCiudadano1 = new Agent("Dani","","dani35@gmail.com","dani123","Ciudadano");
+		Agent agenteCiudadano1_1 = new Agent("Dani","","dani1456@gmail.com","dani123","Ciudadano");
+		Agent agenteCiudadano2 = new Agent("Julio","","julio1235@gmail.com","julio125","Ciudadano");
+		
+		Agent agenteEntidad1 = new Agent("Entidad 1","entidad123@gmail.com","entidad123","Entidad");
+		Agent agenteEntidad1_1 = new Agent("Entidad 1.1","entidad123567@gmail.com","entidad123","Entidad");
+		Agent agenteEntidad2 = new Agent("Entidad 2","entidad123@gmail.com","entidad567","Entidad");
 
-		assertEquals(user1.hashCode(), user2.hashCode());
-		assertEquals(user4.hashCode(), user5.hashCode());
-		assertNotEquals(user2.hashCode(), user3.hashCode());
+		assertEquals(agenteCiudadano1.hashCode(), agenteCiudadano1_1.hashCode());
+		assertNotEquals(agenteCiudadano1.hashCode(), agenteCiudadano2.hashCode());
+		
+		assertEquals(agenteEntidad1.hashCode(), agenteEntidad1_1.hashCode());
+		assertNotEquals(agenteEntidad1.hashCode(), agenteEntidad2.hashCode());
+		
 
-		System.out.println(user1.toString());
 	}
 
 	@Test
-	public void testAll() {
-		Date date = new Date(System.currentTimeMillis());
-		User user1 = new User("Dani", "Duque", "email@gmail.com", date, "C\\Buenavida", "Español", "7777777R");
+	public void testConstructor() {
+		
+		Agent agenteCiudadano1 = new Agent("Dani","","dani35@gmail.com","dani123","Ciudadano");
 
-		String password = user1.getPassword();
-		String userName = user1.getUsername();
-		String toString = "User [id =null, nombre=Dani, apellidos=Duque, email=email@gmail.com, fechaNacimiento=" + date
-				+ ", direccionPostal=C\\Buenavida, nacionalidad=Español, DNI=7777777R]";
+	
+		assertEquals("Dani", agenteCiudadano1.getNombre());
+		assertEquals("email@gmail.com", agenteCiudadano1.getEmail());
+		assertEquals("dani123", agenteCiudadano1.getIdentificador);
+		assertEquals("Ciudadano", agenteCiudadano1.getTipo());
 
-		assertEquals("Dani", user1.getNombre());
-		assertEquals("Duque", user1.getApellidos());
-		assertEquals("email@gmail.com", user1.getEmail());
-		assertEquals(date, user1.getFechaNacimiento());
-		assertEquals("C\\Buenavida", user1.getDireccionPostal());
-		assertEquals("Español", user1.getNacionalidad());
-		assertEquals("7777777R", user1.getDNI());
-		assertEquals(password, user1.getPassword());
-		assertEquals(userName, user1.getUsername());
-		assertEquals(toString, user1.toString());
 	}
 
 }
