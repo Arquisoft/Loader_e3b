a9afbaeacc1da0261a96a2efb82f026f32cb13f0
Manuel Junco <UO252010@uniovi.es>
2018-06-08 13:10:10 +0200

    Test operario

--- src/test/java/modeltest/OperarioTest.java
+++ src/test/java/modeltest/OperarioTest.java
@@ -18,10 +18,32 @@ public class OperarioTest {
 		Operario oper1 = new Operario("oper1@gmail.com","123456");
 		Operario oper2 = new Operario("oper2@gmail.com","123456");
 		Operario oper3 = new Operario("oper3@gmail.com","123456");
+		Operario oper4 = new Operario("oper4@gmail.com","123456","Admin");
 
+		Operario oper5  = new Operario("oper25@gmail.com","123456");
+		
 		assertNotNull(oper1);
 		assertNotNull(oper2);
 		assertNotNull(oper3);
+		assertNotNull(oper4);
+		
+		//compruebo hashCode del ultimo operario
+		assertEquals(oper4.hashCode(), -228767646);
+		
+		//ahora probamos el metodo equals
+		assertTrue(oper1.equals(oper1));
+		assertFalse(oper1.equals(null));
+		assertFalse(oper1.equals( new Agent("Entidad 1",null,"entidad123@gmail.com","entidad123","Entidad")));
+		assertFalse(oper1.equals(oper4));
+		oper1.setRole(null);
+		assertFalse(oper1.equals(oper5));
+		oper1.setPassword(null);
+		assertFalse(oper1.equals(oper5));
+		oper1.setId(null);
+		assertFalse(oper1.equals(oper5));
+		oper1.setEmail(null);
+		assertFalse(oper1.equals(oper5));
+		
 	}
 
 
